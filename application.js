0e13f43a437fbc8c1f96dcb0da6e415189271ab0
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-22 11:15:52 -0500

    peterson_master 1.0  /application.js

--- application.js
+++ application.js
@@ -19,14 +19,13 @@ function renderStoreList(container, template, collection, starter, breaker){
     Mustache.parse(template_html);   // optional, speeds up future uses
     var store_initial="";
     $.each( collection , function( key, val ) {
-        if (type == "stores" || type == "category_stores"){
-            if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
-                val.alt_store_front_url = "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1455899596000/main_logo.png";
-            } else {
-                val.alt_store_front_url = getImageURL(val.store_front_url);    
-            }
-            
+        
+        if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
+            val.alt_store_front_url = "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1455899596000/main_logo.png";
+        } else {
+            val.alt_store_front_url = getImageURL(val.store_front_url);    
         }
+            
         //var categories = getStoreCategories();
         var current_initial = val.name[0];
         
