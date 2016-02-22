15621f9adf5a0e9465bb2ca2d9628ae49e54703a
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-22 11:14:52 -0500

    peterson_master 1.0  /application.js

--- application.js
+++ application.js
@@ -11,3 +11,55 @@ function init(e){
     });
     
 }
+
+function renderStoreList(container, template, collection, type,starter, breaker){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    var store_initial="";
+    $.each( collection , function( key, val ) {
+        if (type == "stores" || type == "category_stores"){
+            if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
+                val.alt_store_front_url = "http://assets.codecloudapp.com/sites/56ba24f16e6f641f99030000/image/png/1446826281000/stc-logo-holiday-360 copy.png";
+            } else {
+                val.alt_store_front_url = getImageURL(val.store_front_url);    
+            }
+            
+        }
+        //var categories = getStoreCategories();
+        var current_initial = val.name[0];
+        
+        if(store_initial.toLowerCase() == current_initial.toLowerCase()){
+            val.initial = "";
+            val.show = "display:none;";
+        }
+        else{
+            val.initial = current_initial;
+            store_initial = current_initial;
+            val.show = "display:block;";
+        }
+        if (val.promotions.length > 0){
+            val.promotion_exist = "display:inline-block";
+        }
+        else{
+            val.promotion_exist = "display:none";
+        }
+        if (val.jobs.length > 0){
+            val.job_exist = "display:inline-block";
+        }
+        else{
+            val.job_exist = "display:none";
+        }
+        val.block = current_initial + '-block';
+        var rendered = Mustache.render(template_html,val);
+        var upper_current_initial = current_initial.toUpperCase();
+        if (upper_current_initial.charCodeAt(0) < breaker.charCodeAt(0) && upper_current_initial.charCodeAt(0) >= starter.charCodeAt(0)){
+            item_rendered.push(rendered);
+        }
+
+    });
+    
+    $(container).show();
+    $(container).html(item_rendered.join(''));
+}
