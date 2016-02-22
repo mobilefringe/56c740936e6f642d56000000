a7919a84ebb942ef7a69d34990434a48b7b03714
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-22 16:12:10 -0500

    peterson_master 1.0  /application.js

--- application.js
+++ application.js
@@ -16,7 +16,7 @@ function init(e){
     
     $('#mobile_cat_list').change(function{
         var v = $(this).val();
-        cat = $('a[data-id="964"]').click()
+        cat = $('a[data-id="' + v + '"]').click()
     })
     
 }
