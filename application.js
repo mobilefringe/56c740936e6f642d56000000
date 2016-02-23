5498fa381691d4398153b6a0a5dba1512e86a9e0
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-23 11:43:31 -0500

    peterson_master 1.0  /application.js

--- application.js
+++ application.js
@@ -40,7 +40,7 @@ function renderPromotions(container, template, collection, mall_name){
             val.image_url = store_details.store_front_url_abs;
         }
         else{
-            val.store_name = "";
+            val.store_name = mall_name;
             val.image_url = "http://assets.codecloudapp.com/sites/56ba0abc6e6f644468020000/image/jpeg/1446753494000/Dixie_default.jpg";
         }
         
