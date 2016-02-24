7450685e929476d7b1ff47d1df16591df9d6e0bd
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-24 15:15:22 -0500

    peterson_master 1.0  /application.js

--- application.js
+++ application.js
@@ -143,18 +143,18 @@ function renderPromoDetails(container, template, collection){
     $.each( item_list , function( key, val ) {
         if (val.promotionable_type == "Store") {
             var store_details = getStoreDetailsByID(val.promotionable_id);
-            val.store_detail_btn = store_details.slug;
+            val.store_detail_btn = store_details.slug ;
             val.store_name = store_details.name;
-            if (store_details.store_front_url_abs.indexOf('missing.png') > -1){
-                val.image_url = "http://assets.codecloudapp.com/sites/56ba0abc6e6f644468020000/image/jpeg/1446753494000/Dixie_default.jpg";
-            }
-            else{
-                val.image_url = store_details.store_front_url_abs;
-            }
+            val.image_url = val.promo_image_url_abs;
+            val.cat_list = store_details.categories.join(',')
         }
         else{
-            val.store_name = "";
-            val.image_url = "http://assets.codecloudapp.com/sites/56ba0abc6e6f644468020000/image/jpeg/1446753494000/Dixie_default.jpg";
+            val.store_name = mall_name;
+            val.image_url = "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+        }
+        
+        if(val.image_url.indexOf('missing.png') > 0){
+            val.image_url  = "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
         }
         
         if(val.promo_image_url_abs.indexOf('missing.png') > -1){
