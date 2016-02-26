30bbc8a7a201c31e04b5a981f8d7f554a98f0c62
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-26 10:57:06 -0500

    peterson_master 1.0  /application.js

--- application.js
+++ application.js
@@ -203,8 +203,6 @@ function renderJobs(container, template, collection, mall_name){
             val.description_short = val.description;
         }
         
-            val.job_type == "Full Time/Part Time"
-        
         var show_date = new Date (val.start_date + "T05:00:00Z");
         val.published_on = get_month(show_date.getMonth()) + " " + show_date.getDate();
         
