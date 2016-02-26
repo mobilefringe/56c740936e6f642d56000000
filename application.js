0fc3b4a6619ed323e09d27afc7d6df38ffbad72b
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-26 10:45:55 -0500

    peterson_master 1.0  /application.js

--- application.js
+++ application.js
@@ -197,7 +197,7 @@ function renderJobs(container, template, collection, mall_name){
             val.store_name = mall_name;
         }
         console.log(val)
-        var show_date = new Date (val.start_Date + "T05:00:00Z");
+        var show_date = new Date (val.start_date + "T05:00:00Z");
         val.published_on = get_month(show_date.getMonth()) + " " + show_date.getDate();
         
         var rendered = Mustache.render(template_html,val);
