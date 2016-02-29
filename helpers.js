297b22fda4b1da6ffa96bd03ee1281a28bdbaa9b
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-29 16:06:22 -0500

    peterson_master 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -165,7 +165,7 @@ function site_search(){
            function(){ $('body').addClass('no_scroll') },
            function(){ $('body').removeClass('no_scroll') }
     )
-    $('#site_search').keyup(function(){
+    $('.site_search').keyup(function(){
         if ($('#site_search').val() == ""){
             $('#search_results_stores').html('');
             $('#search_results_events').html('');
