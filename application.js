403875e6d4cb8d2954a5135f3dd9550f2f199a89
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-03-03 15:12:07 -0500

    peterson_master 1.0  /application.js

--- application.js
+++ application.js
@@ -1,47 +1,55 @@
-/*Created 2015-02-28 by CodeCloud Team*/
 
-function init(e){
-    $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
-    $('.open_menu').click(function(e){
-        $('body').addClass('no_scroll');
-        $('.mobile_menu_container').fadeIn();
-    });
-    $('#close_menu').click(function(e){
-        $('body').removeClass('no_scroll');
-        $('.mobile_menu_container').fadeOut();
-    });
-    
-    $('#mobile_alpha_list').change(function(){
-        window.location.href = "#" + $(this).val().toUpperCase();
-    })
-    
-    $('#mobile_cat_list').change(function(){
-        var v = $(this).val();
-        if(v == "all"){
-            $('.show_all_stores').click();
-       }
-       else{
-           $('a[data-id="' + v + '"]').click();
-       }
-     
-    })
-    
-    $('.locate_store').click(function(e){
-        e.preventDefault();
-        $('.stores_table').show()
-    })
-    site_search();
-    $('.search_btn').click(function(){
-        $('.mobile_search').fadeIn();
-        $('#site_search').focus()
-    })
-    $('.close_search').click(function(){
-        $('.mobile_search').fadeOut();
-        $('.search_results_container').fadeOut();
-        $('#site_search').val('')
-    })
-    
 
+function renderBanner(banner_template,home_banner,banners){
+    var item_list = [];
+    var item_rendered = [];
+    var banner_template_html = $(banner_template).html();
+    Mustache.parse(banner_template_html);   // optional, speeds up future uses
+    $.each( banners , function( key, val ) {
+        today = new Date();
+        start = new Date (val.start_date);
+       
+        start.setDate(start.getDate());
+       if(val.url == "" || val.url === null){
+           val.css = "style=cursor:default;";
+           val.noLink = "return false";
+       }
+       if (start <= today){
+         if (val.end_date){
+             end = new Date (val.end_date);
+             end.setDate(end.getDate() + 1);
+             if (end >= today){
+               item_list.push(val);  
+             }
+             
+         } else {
+             item_list.push(val);
+         }
+       }
+    });
+
+    $.each( item_list , function( key, val ) {
+        var repo_rendered = Mustache.render(banner_template_html,val);
+        item_rendered.push(repo_rendered);
+       
+    });
+    $(home_banner).html(item_rendered.join(''));
+    
+}
+function renderFeatureItems(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        if(val.url == "" || val.url === null){
+           val.css = "style=cursor:default;";
+           val.noLink = "return false";
+        }
+        var repo_rendered = Mustache.render(template_html,val);
+        item_rendered.push(repo_rendered);
+    });
+    $(container).html(item_rendered.join(''));
 }
 
 function renderPromotions(container, template, collection){
