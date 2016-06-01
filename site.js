4dfa041ff5f747231e37a79ce17de181e2de6eb2
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-01 13:48:15 -0400

    peterson_master 1.0  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json = {
-                    "name" : "The District at Green Valley Ranch",
-                    "default_image" : "http://assets.codecloudapp.com/sites/5730acef6e6f6455e0000000/image/jpeg/1463757854000/dgvr_500x500_default.jpg",
+                    "name" : "Virginia Gateway",
+                    "default_image" : "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1455899596000/main_logo.png",
                     "time_zone" : "T08:00:00Z",
                     "instagram" : "196558634"
                 }
