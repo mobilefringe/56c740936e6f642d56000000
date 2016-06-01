b833bc3a0d80e4fbdfb1942918a65f5d6b53a99e
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-01 13:50:36 -0400

    peterson_master 1.0  /site.js

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 var site_json = {
-                    "name" : "Virginia Gateway",
+                    "mall_name" : "Virginia Gateway",
                     "default_image" : "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1455899596000/main_logo.png",
                     "time_zone" : "T05:00:00Z",
                 }
