1943f5edc11f048fbca19e8650ba52538a064998
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-11 15:48:48 -0400

    peterson_master 1.0  /site.js

--- site.js
+++ site.js
@@ -2,6 +2,6 @@ var site_json = {
                     "mall_name" : "Virginia Gateway",
                     "default_image" : "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1455899596000/main_logo.png",
                     "time_zone" : "T05:00:00Z",
-                    "newsletter_form_url" : "http://petersoncompanies.createsend.com/t/d/s/ftutr/",
+                    "newsletter_form_url" : "http://petersoncompanies.createsend.com/t/d/s/ftutr/?callback=?",
                     "newsletter_email_field" : "cm-ftutr-ftutr"
                 }
