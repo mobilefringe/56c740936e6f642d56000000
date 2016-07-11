b222e3844bef7a39de288494bdc2d6594f8124ec
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-11 12:01:02 -0400

    peterson_master 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -301,7 +301,11 @@ function site_search(){
 function renderSitePopup(){
     var pp = getPopups()[0];
     if (pp != undefined){
+        if (pp.photo_link.length > 0){
+            $('.popup-body').html('<a id="pp_url" href="' + pp.photo_link +'"><img id="pp_img" src="' + pp.photo_url + '" class="" alt=""></a>')
+        }
         $('#pp_img').attr('src', 'http://cdn.mallmaverick.com' +pp.photo_url)
+        
         $('#pp_url').attr('href',  pp.photo_link)
     
         if($.cookie("popup_viewed") != "true"){
