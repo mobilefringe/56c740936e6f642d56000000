0d799f3dac5223926d2f5c2a23ebfacbf34c7563
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-11 11:57:05 -0400

    peterson_master 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -44,7 +44,7 @@ function init(e){
     
     $('.popup-close').click(function(){
         $('.hidden-popup-bg').hide()
-        $('body').removeClass('no_scroll')
+        $('body').removeClass('no_scroll');
     })
     
 
